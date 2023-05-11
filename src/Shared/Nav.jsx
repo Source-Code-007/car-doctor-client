1d7bd7bcf6d678a8ae780cf83d43b2100347babf
Source-Code-007 <utsho926@gmail.com>
2023-05-11 18:11:11 +0600

    navbar modified

--- src/Shared/Nav.jsx
+++ src/Shared/Nav.jsx
@@ -1,4 +1,14 @@
+import { FaCartPlus, FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Nav = () => {
+    const navItem = <>
+        <li className="font-bold"><Link to={'/'}>Home</Link></li>
+        <li className="font-bold"><Link to={'/about'}>About</Link></li>
+        <li className="font-bold"><Link>Services</Link></li>
+        <li className="font-bold"><Link>Blog</Link></li>
+        <li className="font-bold"><Link>Contact</Link></li>
+    </>
     return (
         <nav className="navbar shadow">
             <div className="navbar-start">
@@ -7,40 +17,20 @@ const Nav = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <a className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navItem}
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">Car Doctor</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <a>
-                            Parent
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </a>
-                        <ul className="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {navItem}
                 </ul>
             </div>
-            <div className="navbar-end">
-                <a className="btn">Get started</a>
+            <div className="navbar-end gap-3">
+                <span><FaCartPlus></FaCartPlus></span>
+                <span><FaSearch></FaSearch></span>
+                <button className="btn btn-outline btn-error">Appointment</button>
             </div>
         </nav>
     );
