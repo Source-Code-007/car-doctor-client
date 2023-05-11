14dab32fee90ed85a1036525c10fead54b826530
Source-Code-007 <utsho926@gmail.com>
2023-05-11 11:54:33 +0600

    banner button reposition

--- src/Homepage/Banner/Banner.jsx
+++ src/Homepage/Banner/Banner.jsx
@@ -1,30 +1,35 @@
+import bannerOne from "../../assets/images/banner/1.jpg";
+import bannerTwo from "../../assets/images/banner/2.jpg";
+import bannerThree from "../../assets/images/banner/3.jpg";
+import bannerFour from "../../assets/images/banner/4.jpg";
+
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full h-[calc(100vh-60px)]">
             <div id="slide1" className="carousel-item relative w-full">
-                <img src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <img src={bannerOne} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 gap-4 right-8 bottom-5">
                     <a href="#slide4" className="btn btn-circle">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-                <img src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <img src={bannerTwo} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 gap-4 right-8 bottom-5">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-                <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <img src={bannerThree} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 gap-4 right-8 bottom-5">
                     <a href="#slide2" className="btn btn-circle">❮</a>
                     <a href="#slide4" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full">
-                <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <img src={bannerFour} className="w-full" />
+                <div className="absolute flex justify-end transform -translate-y-1/2 gap-4 right-8 bottom-5">
                     <a href="#slide3" className="btn btn-circle">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
                 </div>
