88a10c3e697d306de6ab99b65ecabf6bdf27226c
Source-Code-007 <utsho926@gmail.com>
2023-05-22 22:10:00 +0600

    search and filtering option added in services section in homepage

--- src/Homepage/Services/Services.jsx
+++ src/Homepage/Services/Services.jsx
@@ -17,6 +17,20 @@ const Services = () => {
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.</p>
             </div>
 
+            <div className="form-control">
+                <div className="input-group">
+                    <input type="text" placeholder="Search…" className="input input-bordered" />
+                    <button className="btn btn-square">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+                </div>
+            </div>
+            <select className="select select-error w-full max-w-xs">
+                <option disabled selected>What is the best headless CMS</option>
+                <option>High to Low</option>
+                <option>Low to High</option>
+            </select>
+
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
                 {
                     services.map(service => {
